1b30b7224cfc9d94bc619bef1eb309e3e469f59d
Hlib Hryshko <glebg27@gmail.com>
2024-09-24 15:47:03 +0200

    person model interface is exported

--- server/src/models/Person.ts
+++ server/src/models/Person.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, Document } from "mongoose";
 
-interface IPerson extends Document {
+export interface IPerson extends Document {
   name: string;
   email: string;
   password: string;
