36a4266fe51bd6bf50023acb80448cf8c4c2016e
Hlib Hryshko <glebg27@gmail.com>
2024-09-25 20:43:09 +0200

    auth middleware is added

---
+++ server/src/middleware/authMiddleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface AuthRequest extends Request {
+  user?: string | object;
+}
+
+export const protect = (req: AuthRequest, res: Response, next: NextFunction) => {
+  let token;
+
+  // Check if the Authorization header is present and the token starts with "Bearer"
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1]; // Get the token part
+
+    try {
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+
+      // Attach the user data (decoded token) to the request object
+      req.user = decoded;
+
+      next(); // Call the next middleware
+    } catch (error) {
+      return res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: 'Not authorized, no token' });
+  }
+};
