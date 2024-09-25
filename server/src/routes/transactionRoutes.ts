b6a5aab9b1d65db520698ba59e4e1bc63a6d3411
Hlib Hryshko <glebg27@gmail.com>
2024-09-25 21:46:07 +0200

    get all transactions route is created

--- server/src/routes/transactionRoutes.ts
+++ server/src/routes/transactionRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createTransaction } from "../controllers/transactionController";
+import {
+  createTransaction,
+  getAllTransactions,
+} from "../controllers/transactionController";
 import { protect } from "../middleware/authMiddleware";
 
 const router = Router();
 
 router.post("/", protect, createTransaction);
+router.get("/", protect, getAllTransactions);
 
 export default router;
