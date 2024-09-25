8a0e8ffe575d9a015d2d8ea3a1d8e1c48d90b283
Hlib Hryshko <glebg27@gmail.com>
2024-09-25 21:21:22 +0200

    an icon and a list of transactions is added to the category model

--- server/src/models/Category.ts
+++ server/src/models/Category.ts
@@ -2,10 +2,14 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface ICategory extends Document {
   name: string;
+  icon: string;
+  transactions: mongoose.Types.ObjectId[];
 }
 
 const CategorySchema: Schema = new Schema({
   name: { type: String, required: true },
+  icon: { type: String, required: true },
+  transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Transaction" }],
 });
 
 export const Category = mongoose.model<ICategory>("Category", CategorySchema);
