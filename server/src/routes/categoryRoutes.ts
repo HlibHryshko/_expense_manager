6e4f6d146ac7588ceee260c783085d7e8a9171bb
Hlib Hryshko <glebg27@gmail.com>
2024-09-25 22:18:09 +0200

    get category expenses route is added

--- server/src/routes/categoryRoutes.ts
+++ server/src/routes/categoryRoutes.ts
@@ -3,11 +3,13 @@ import { protect } from "../middleware/authMiddleware";
 import {
   createCategory,
   getAllCategories,
+  getCategoryExpenses,
 } from "../controllers/categoryController";
 
 const router = Router();
 
 router.post("/", protect, createCategory);
 router.get("/", protect, getAllCategories);
+router.get("/expenses", protect, getCategoryExpenses);
 
 export default router;
