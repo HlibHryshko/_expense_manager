236ced6191ca6d349678a62989ca6c35bb939880
Hlib Hryshko <glebg27@gmail.com>
2024-09-25 20:43:12 +0200

    ammendment

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -1,31 +1,38 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
 interface AuthRequest extends Request {
-  user?: string | object;
+  user?: string;
 }
 
-export const protect = (req: AuthRequest, res: Response, next: NextFunction) => {
+export const protect = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
   let token;
 
   // Check if the Authorization header is present and the token starts with "Bearer"
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    token = req.headers.authorization.split(' ')[1]; // Get the token part
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1]; // Get the token part
 
     try {
       // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET!);
 
       // Attach the user data (decoded token) to the request object
-      req.user = decoded;
+      req.user = decoded.toString();
 
       next(); // Call the next middleware
     } catch (error) {
-      return res.status(401).json({ message: 'Not authorized, token failed' });
+      return res.status(401).json({ message: "Not authorized, token failed" });
     }
   }
 
   if (!token) {
-    return res.status(401).json({ message: 'Not authorized, no token' });
+    return res.status(401).json({ message: "Not authorized, no token" });
   }
 };
