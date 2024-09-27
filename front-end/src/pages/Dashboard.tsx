127da2f03946cd07e04e2b8cb49d362113c59d1b
Hlib Hryshko <glebg27@gmail.com>
2024-09-27 19:36:42 +0200

    expenses -> transactions

--- front-end/src/pages/Dashboard.tsx
+++ front-end/src/pages/Dashboard.tsx
@@ -1,11 +1,11 @@
 // src/pages/Dashboard.tsx
-import ExpenseList from "../components/ExpenseList";
+import TransactionsList from "../components/TransactionsList";
 
 const Dashboard = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-8">Expense Tracker Dashboard</h1>
-      <ExpenseList />
+      <TransactionsList />
     </div>
   );
 };
