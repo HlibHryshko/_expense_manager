e42891f9a94f0a89c6d3a6a4f9cbd78d11757cd2
Hlib Hryshko <glebg27@gmail.com>
2024-09-27 19:24:21 +0200

    dashboard page is added to the app component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,11 @@
 import CategoriesWithChart from "./components/CategoriesWithChart";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div className="container mx-auto p-4">
       <CategoriesWithChart />
+      <Dashboard />
     </div>
   );
 }
