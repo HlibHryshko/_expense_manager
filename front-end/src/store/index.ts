480bafc78f34c81d51a3407c00b4623f5f24fb28
Hlib Hryshko <glebg27@gmail.com>
2024-09-27 19:23:35 +0200

    expenses slice is added to the store

--- front-end/src/store/index.ts
+++ front-end/src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriesReducer from "./slices/categoriesSlice";
+import expensesReducer from "./slices/expensesSlice";
 
 export const store = configureStore({
   reducer: {
     categories: categoriesReducer,
+    expenses: expensesReducer,
   },
 });
 
