30001eace7f1daa6f89f9a0fb0dc16eb3a754f14
Hlib Hryshko <glebg27@gmail.com>
2024-10-04 00:42:08 +0200

    react router is used in the app component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,16 @@
-import CategoriesWithChart from "./components/CategoriesWithChart";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
-    <div className="container mx-auto p-4">
-      <CategoriesWithChart />
-      <Dashboard />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
