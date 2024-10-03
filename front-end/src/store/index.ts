faee184ab2035f815a2ee69807738a425c281f96
Hlib Hryshko <glebg27@gmail.com>
2024-10-04 00:41:53 +0200

    auth slice is added to the store

--- front-end/src/store/index.ts
+++ front-end/src/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import categoriesReducer from "./slices/categoriesSlice";
 import transactionsReducer from "./slices/transactionSlice";
 import expensesReducer from "./slices/expensesSlice";
+import authReducer from "./slices/authSlice";
 
 export const store = configureStore({
   reducer: {
     categories: categoriesReducer,
     transactions: transactionsReducer,
     expenses: expensesReducer,
+    auth: authReducer,
   },
 });
 
