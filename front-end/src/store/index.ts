9bf6c332fb8f1f943f380fcb119d08145f836cf8
Hlib Hryshko <glebg27@gmail.com>
2024-10-04 00:00:13 +0200

    expenses slice is used in redux store index file

--- front-end/src/store/index.ts
+++ front-end/src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriesReducer from "./slices/categoriesSlice";
 import transactionsReducer from "./slices/transactionSlice";
+import expensesReducer from "./slices/expensesSlice";
 
 export const store = configureStore({
   reducer: {
     categories: categoriesReducer,
     transactions: transactionsReducer,
+    expenses: expensesReducer,
   },
 });
 
