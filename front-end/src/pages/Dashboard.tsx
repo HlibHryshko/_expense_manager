e552d44b04cce3eceb0aa0a1f2e0912ab76e3162
Hlib Hryshko <glebg27@gmail.com>
2024-10-04 21:40:37 +0200

    logout button is added to the dashboard page

--- front-end/src/pages/Dashboard.tsx
+++ front-end/src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 // src/pages/Dashboard.tsx
+import LogoutButton from "../components/LogoutButton";
 import TransactionForm from "../components/TransactionForm";
 import TransactionsList from "../components/TransactionsList";
 
 const Dashboard = () => {
   return (
     <div className="container mx-auto p-4">
+      <LogoutButton />
       <h1 className="text-3xl font-bold mb-8">Expense Tracker Dashboard</h1>
       <TransactionsList />
       <TransactionForm />
