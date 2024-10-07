3c9fc6151f7f75ae3c0620eb937261d88a0db0a2
Hlib Hryshko <glebg27@gmail.com>
2024-10-08 01:58:22 +0200

    sorting is added to the sorted transactions table

--- front-end/src/components/SortedTransactionsTable.tsx
+++ front-end/src/components/SortedTransactionsTable.tsx
@@ -1,42 +1,69 @@
+import { GoArrowDown, GoArrowUp } from "react-icons/go";
+import useSort from "../hooks/useSort";
 import { Transaction } from "../store/slices/transactionSlice";
 import { TransactionsConfig } from "./TransactionsList";
+import TransactionsTable from "./TransactionsTable";
 
 interface SortedTransactionsTableProps {
   transactions: Transaction[];
   config: TransactionsConfig[];
+  generateKey: (transaction: Transaction) => string;
 }
 
 const SortedTransactionsTable = ({
+  config,
   transactions,
+  generateKey,
 }: SortedTransactionsTableProps) => {
+  const { setSortLabel, sortedData, sortBy, sortOrder } = useSort(
+    transactions,
+    config
+  );
+
+  const updatedConfig: TransactionsConfig[] = config.map((configData) => {
+    if (!configData.sortValue) {
+      return configData;
+    }
+
+    return {
+      ...configData,
+      header: () => (
+        <th
+          className="cursor-pointer hover:bg-gray-100"
+          onClick={() => {
+            setSortLabel(configData.label);
+          }}
+        >
+          <div className="flex items-centered">
+            {sortBy === configData.label ? (
+              sortOrder === "asc" ? (
+                <div>
+                  <GoArrowUp />
+                </div>
+              ) : (
+                <div>
+                  <GoArrowDown />
+                </div>
+              )
+            ) : (
+              <div>
+                <GoArrowUp />
+                <GoArrowDown />
+              </div>
+            )}
+            {configData.label}
+          </div>
+        </th>
+      ),
+    };
+  });
+
   return (
-    <table className="min-w-full bg-white border-collapse">
-      <thead>
-        <tr>
-          <th className="py-2 border-b">Description</th>
-          <th className="py-2 border-b">Amount</th>
-          <th className="py-2 border-b">Date</th>
-          <th className="py-2 border-b">Category</th>
-        </tr>
-      </thead>
-      <tbody>
-        {transactions &&
-          transactions.map((transaction) => (
-            <tr key={transaction._id}>
-              <td className="py-2 px-4 border-b">{transaction.description}</td>
-              <td className="py-2 px-4 border-b">
-                ${transaction.amount.toFixed(2)}
-              </td>
-              <td className="py-2 px-4 border-b">
-                {new Date(transaction.date).toLocaleDateString()}
-              </td>
-              <td className="py-2 px-4 border-b">
-                {transaction.category.name}
-              </td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
+    <TransactionsTable
+      generateKey={generateKey}
+      transactions={sortedData}
+      config={updatedConfig}
+    />
   );
 };
 
