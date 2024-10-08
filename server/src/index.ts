72091d1acf8f3422a9e2667667c2b08a139c45ce
Hlib Hryshko <glebg27@gmail.com>
2024-10-08 03:09:18 +0200

    passport middleware is used in the main file

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,8 @@ import transactionRoutes from "./routes/transactionRoutes";
 import categoryRoutes from "./routes/categoryRoutes";
 import connectToDB from "./config/db_configuration";
 import cors from "cors";
+import passport from "passport";
+import session from "express-session";
 
 // Load environment variables
 dotenv.config();
@@ -17,6 +19,21 @@ app.use(express.json()); // For parsing JSON requests
 
 app.use(cors());
 
+// Express session setup
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "your_secret_key", // secret is required
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: process.env.NODE_ENV === "production" }, // Cookies will only be sent over HTTPS in production
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Auth routes
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/categories", categoryRoutes);
