8c524e21aba23a6e4b14aab757bfd3a9c849859f
Hlib Hryshko <glebg27@gmail.com>
2024-10-08 03:10:56 +0200

    google oauth is configured in the main file

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -4,11 +4,17 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/index.ts";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const googleClientId =
+  "241449959544-pr8c4vrgco13m4u21di7ti08q10mb6lu.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <GoogleOAuthProvider clientId={googleClientId}>
+        <App />
+      </GoogleOAuthProvider>
     </Provider>
   </StrictMode>
 );
