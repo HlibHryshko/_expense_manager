70b2e26058b4d71b2b0886909592f4dc168a8c4e
Hlib Hryshko <glebg27@gmail.com>
2024-10-16 19:59:20 +0200

    oauth login is added + check if the password is present is added

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { IPerson, Person } from "../models/Person";
 import bcrypt from "bcryptjs";
 import { signJwt } from "../utils/jwt";
+import { OAuth2Client } from "google-auth-library";
 
 interface RegisterRequest {
   name: string;
@@ -13,6 +14,8 @@ interface LoginRequest {
   email: string;
   password: string;
 }
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 export const registerUser = async (req: Request, res: Response) => {
   const { name, email, password } = req.body as RegisterRequest;
@@ -44,6 +47,12 @@ export const loginUser = async (req: Request, res: Response) => {
       return res.status(400).json({ message: "User not found" });
     }
 
+    if (!user.password) {
+      return res
+        .status(400)
+        .json({ message: "This user is registered through Google Account" });
+    }
+
     // Compare the password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
@@ -57,5 +66,52 @@ export const loginUser = async (req: Request, res: Response) => {
     res.json({ token });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const loginOauthUser = async (req: Request, res: Response) => {
+  const { token } = req.body;
+
+  try {
+    // Verify the token using Google OAuth2 client
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID, // Ensure this matches your frontend client ID
+    });
+
+    const payload = ticket.getPayload();
+
+    if (!payload) {
+      return res
+        .status(400)
+        .json({ error: "Google token verification failed" });
+    }
+
+    const { email, sub: googleId, name } = payload;
+
+    // Check if a user with this Google ID already exists
+    let user = await Person.findOne({ googleId });
+
+    if (!user) {
+      // If not, check if a user with this email exists
+      user = await Person.findOne({ email });
+
+      if (user) {
+        // Link Google account to existing user by adding googleId
+        user.googleId = googleId;
+      } else {
+        // If no user exists, create a new one
+        user = new Person({ email, googleId, name });
+      }
+
+      await user.save();
+    }
+
+    // Generate JWT
+    const jwtToken = signJwt(user._id!.toString());
+    return res.json({ token: jwtToken, user });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
